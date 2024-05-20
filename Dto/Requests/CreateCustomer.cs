cbe9ce8117c3655adbf1e7222e19f7cb16a22c58
Oskar Johansson <oskar.tzar@hotmail.com>
2024-05-20 16:05:13 +0200

    Klar!

--- Dto/Requests/CreateCustomer.cs
+++ Dto/Requests/CreateCustomer.cs
@@ -6,7 +6,6 @@
         public string Phone { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
         public string Password { get; set; }
     }
 }
